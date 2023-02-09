71bee60d0a4d0e1ed1a04731166799eddf544657
Giulian Biolo <giulianbiolo@gmail.com>
2023-02-09 19:56:27 +0100

    Implemented parsing of SphereArrays

--- src/parser.rs
+++ src/parser.rs
@@ -66,80 +66,105 @@ pub fn parse_yaml_scene(filename: &str) -> HittableList {
     for hashobj in hashworld {
         let hashobj = hashobj.as_hash().unwrap();
         let objtype = hashobj[&yaml_rust::Yaml::String("objType".to_string())].as_str().unwrap();
-        let objmat = hashobj[&yaml_rust::Yaml::String("material".to_string())].as_hash().unwrap();
-        let objmattype = objmat[&yaml_rust::Yaml::String("matType".to_string())].as_str().unwrap();
-        // For each kind of object we have different properties (attributes)
-        let material: Box<dyn Material>;
-        match objmattype {
-            "Lambertian" => {
-                // has just an albedo
-                let albedo = objmat[&yaml_rust::Yaml::String("albedo".to_string())].as_vec().unwrap();
-                material = Box::new(Lambertian::new(Color::new(albedo[0].as_f64().unwrap() as f32, albedo[1].as_f64().unwrap() as f32, albedo[2].as_f64().unwrap() as f32)));
-            },
-            "Metal" => {
-                // has an albedo and a fuzz
-                let albedo = objmat[&yaml_rust::Yaml::String("albedo".to_string())].as_vec().unwrap();
-                let fuzz = objmat[&yaml_rust::Yaml::String("fuzz".to_string())].as_f64().unwrap();
-                material = Box::new(Metal::new(Color::new(albedo[0].as_f64().unwrap() as f32, albedo[1].as_f64().unwrap() as f32, albedo[2].as_f64().unwrap() as f32), fuzz as f32));
-            },
-            "Dielectric" => {
-                // has just an index of refraction
-                let ior = objmat[&yaml_rust::Yaml::String("refractionIdx".to_string())].as_f64().unwrap();
-                material = Box::new(Dielectric::new(ior as f32));
-            },
-            "DiffuseLight" => {
-                // has just an emittance
-                let emittance = objmat[&yaml_rust::Yaml::String("emittance".to_string())].as_vec().unwrap();
-                material = Box::new(DiffuseLight::new(Color::new(emittance[0].as_f64().unwrap() as f32, emittance[1].as_f64().unwrap() as f32, emittance[2].as_f64().unwrap() as f32)));
+        if !objtype.contains("Array") {
+            let material: Box<dyn Material + Send + Sync> = _parse_material(hashobj);
+            let obj: Box<dyn Hittable + Send + Sync> = _parse_geometry(hashobj, material);
+            world.push(obj);
+        } else if objtype.contains("Sphere") {
+            // * SphereArray *
+            // ! In future we will support other objects
+            let objects = hashobj[&yaml_rust::Yaml::String("objects".to_string())].as_vec().unwrap();
+            let mut spheres: Vec<Sphere> = Vec::<Sphere>::new();
+            for obj in objects {
+                let obj = obj.as_hash().unwrap();
+                let objtype = obj[&yaml_rust::Yaml::String("objType".to_string())].as_str().unwrap();
+                if objtype == "Sphere" {
+                    let center = obj[&yaml_rust::Yaml::String("center".to_string())].as_vec().unwrap();
+                    let radius = obj[&yaml_rust::Yaml::String("radius".to_string())].as_f64().unwrap();
+                    let material = _parse_material(obj);
+                    spheres.push(Sphere::new(Point3::new(center[0].as_f64().unwrap() as f32, center[1].as_f64().unwrap() as f32, center[2].as_f64().unwrap() as f32), radius as f32, material, 0));
+                }
             }
-            _ => { panic!("Unknown object type: {}", objtype); }
-        };
-        let obj: Box<dyn Hittable + Send + Sync>;
-        match objtype {
-            "Sphere" => {
-                // has a center and radius
-                let center = hashobj[&yaml_rust::Yaml::String("center".to_string())].as_vec().unwrap();
-                let radius = hashobj[&yaml_rust::Yaml::String("radius".to_string())].as_f64().unwrap();
-                obj = Box::new(Sphere::new(Point3::new(center[0].as_f64().unwrap() as f32, center[1].as_f64().unwrap() as f32, center[2].as_f64().unwrap() as f32), radius as f32, material, 0));
-            },
-            "Triangle" => {
-                // has an array of 3 arrays (the vertices)
-                let vertices = hashobj[&yaml_rust::Yaml::String("vertices".to_string())].as_vec().unwrap();
-                let v0 = vertices[0].as_vec().unwrap();
-                let v1 = vertices[1].as_vec().unwrap();
-                let v2 = vertices[2].as_vec().unwrap();
-                // obj = Box::new(Triangle::new(Point3::new(v0[0].as_f64().unwrap() as f32, v0[1].as_f64().unwrap() as f32, v0[2].as_f64().unwrap() as f32), Point3::new(v1[0].as_f64().unwrap() as f32, v1[1].as_f64().unwrap() as f32, v1[2].as_f64().unwrap() as f32), Point3::new(v2[0].as_f64().unwrap() as f32, v2[1].as_f64().unwrap() as f32, v2[2].as_f64().unwrap() as f32), material, 0));
-                obj = Box::new(
-                    Triangle::new(
-                        [
-                            Vec3A::new(v0[0].as_f64().unwrap() as f32, v0[1].as_f64().unwrap() as f32, v0[2].as_f64().unwrap() as f32),
-                            Vec3A::new(v1[0].as_f64().unwrap() as f32, v1[1].as_f64().unwrap() as f32, v1[2].as_f64().unwrap() as f32),
-                            Vec3A::new(v2[0].as_f64().unwrap() as f32, v2[1].as_f64().unwrap() as f32, v2[2].as_f64().unwrap() as f32)
-                        ],
-                        material,
-                        0
-                    )
-                );
-            },
-            "Mesh" => {
-                // has a filename, position, rotation and scale
-                let filename = hashobj[&yaml_rust::Yaml::String("filename".to_string())].as_str().unwrap();
-                let position = hashobj[&yaml_rust::Yaml::String("position".to_string())].as_vec().unwrap();
-                let rotation = hashobj[&yaml_rust::Yaml::String("rotation".to_string())].as_vec().unwrap();
-                let scale = hashobj[&yaml_rust::Yaml::String("scalingFactor".to_string())].as_f64().unwrap();
-                obj = Box::new(
-                    Mesh::new(
-                        Vec3A::new(position[0].as_f64().unwrap() as f32, position[1].as_f64().unwrap() as f32, position[2].as_f64().unwrap() as f32),
-                        scale as f32,
-                        Vec3A::new(rotation[0].as_f64().unwrap() as f32, rotation[1].as_f64().unwrap() as f32, rotation[2].as_f64().unwrap() as f32),
-                        filename,
-                        material
-                    )
-                );
-            },
-            _ => { panic!("Unknown object type: {}", objtype); }
-        }
-        world.push(obj);
+            let spherearray = SphereArray::new(&mut spheres);
+            world.push(Box::new(spherearray));
+        } else { panic!("Unsupported object type: {}", objtype) }
     }
     world
+}
+
+fn _parse_material(hashobj: &yaml_rust::yaml::Hash) -> Box<dyn Material + Send + Sync> {
+    let objmat = hashobj[&yaml_rust::Yaml::String("material".to_string())].as_hash().unwrap();
+    let objmattype = objmat[&yaml_rust::Yaml::String("matType".to_string())].as_str().unwrap();
+    match objmattype {
+        "Lambertian" => {
+            // has just an albedo
+            let albedo = objmat[&yaml_rust::Yaml::String("albedo".to_string())].as_vec().unwrap();
+            Box::new(Lambertian::new(Color::new(albedo[0].as_f64().unwrap() as f32, albedo[1].as_f64().unwrap() as f32, albedo[2].as_f64().unwrap() as f32)))
+        },
+        "Metal" => {
+            // has an albedo and a fuzz
+            let albedo = objmat[&yaml_rust::Yaml::String("albedo".to_string())].as_vec().unwrap();
+            let fuzz = objmat[&yaml_rust::Yaml::String("fuzz".to_string())].as_f64().unwrap();
+            Box::new(Metal::new(Color::new(albedo[0].as_f64().unwrap() as f32, albedo[1].as_f64().unwrap() as f32, albedo[2].as_f64().unwrap() as f32), fuzz as f32))
+        },
+        "Dielectric" => {
+            // has just an index of refraction
+            let ior = objmat[&yaml_rust::Yaml::String("refractionIdx".to_string())].as_f64().unwrap();
+            Box::new(Dielectric::new(ior as f32))
+        },
+        "DiffuseLight" => {
+            // has just an emittance
+            let emittance = objmat[&yaml_rust::Yaml::String("emittance".to_string())].as_vec().unwrap();
+            Box::new(DiffuseLight::new(Color::new(emittance[0].as_f64().unwrap() as f32, emittance[1].as_f64().unwrap() as f32, emittance[2].as_f64().unwrap() as f32)))
+        }
+        _ => { panic!("Unknown material type: {:?}", objmat); }
+    }
+}
+
+fn _parse_geometry(hashobj: &yaml_rust::yaml::Hash, material: Box<dyn Material + Send + Sync>) -> Box<dyn Hittable + Send + Sync> {
+    let objtype = hashobj[&yaml_rust::Yaml::String("objType".to_string())].as_str().unwrap();
+    match objtype {
+        "Sphere" => {
+            // has a center and radius
+            let center = hashobj[&yaml_rust::Yaml::String("center".to_string())].as_vec().unwrap();
+            let radius = hashobj[&yaml_rust::Yaml::String("radius".to_string())].as_f64().unwrap();
+            Box::new(Sphere::new(Point3::new(center[0].as_f64().unwrap() as f32, center[1].as_f64().unwrap() as f32, center[2].as_f64().unwrap() as f32), radius as f32, material, 0))
+        },
+        "Triangle" => {
+            // has an array of 3 arrays (the vertices)
+            let vertices = hashobj[&yaml_rust::Yaml::String("vertices".to_string())].as_vec().unwrap();
+            let v0 = vertices[0].as_vec().unwrap();
+            let v1 = vertices[1].as_vec().unwrap();
+            let v2 = vertices[2].as_vec().unwrap();
+            // obj = Box::new(Triangle::new(Point3::new(v0[0].as_f64().unwrap() as f32, v0[1].as_f64().unwrap() as f32, v0[2].as_f64().unwrap() as f32), Point3::new(v1[0].as_f64().unwrap() as f32, v1[1].as_f64().unwrap() as f32, v1[2].as_f64().unwrap() as f32), Point3::new(v2[0].as_f64().unwrap() as f32, v2[1].as_f64().unwrap() as f32, v2[2].as_f64().unwrap() as f32), material, 0));
+            Box::new(
+                Triangle::new(
+                    [
+                        Vec3A::new(v0[0].as_f64().unwrap() as f32, v0[1].as_f64().unwrap() as f32, v0[2].as_f64().unwrap() as f32),
+                        Vec3A::new(v1[0].as_f64().unwrap() as f32, v1[1].as_f64().unwrap() as f32, v1[2].as_f64().unwrap() as f32),
+                        Vec3A::new(v2[0].as_f64().unwrap() as f32, v2[1].as_f64().unwrap() as f32, v2[2].as_f64().unwrap() as f32)
+                    ],
+                    material,
+                    0
+                )
+            )
+        },
+        "Mesh" => {
+            // has a filename, position, rotation and scale
+            let filename = hashobj[&yaml_rust::Yaml::String("filename".to_string())].as_str().unwrap();
+            let position = hashobj[&yaml_rust::Yaml::String("position".to_string())].as_vec().unwrap();
+            let rotation = hashobj[&yaml_rust::Yaml::String("rotation".to_string())].as_vec().unwrap();
+            let scale = hashobj[&yaml_rust::Yaml::String("scalingFactor".to_string())].as_f64().unwrap();
+            Box::new(
+                Mesh::new(
+                    Vec3A::new(position[0].as_f64().unwrap() as f32, position[1].as_f64().unwrap() as f32, position[2].as_f64().unwrap() as f32),
+                    scale as f32,
+                    Vec3A::new(rotation[0].as_f64().unwrap() as f32, rotation[1].as_f64().unwrap() as f32, rotation[2].as_f64().unwrap() as f32),
+                    filename,
+                    material
+                )
+            )
+        },
+        _ => { panic!("Unknown object type: {}", objtype); }
+    }
 }
